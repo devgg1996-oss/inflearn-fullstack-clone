b267dfb36495e3c8a789cf11a97536f0158bd692
devgg1996 <devgg1996@gmail.com>
2025-12-25 14:54:27 +0900

    be - setting - package.json

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,19 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrismaClient } from '../generated/prisma';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    const prisma = new PrismaClient();
-    const test = prisma.test.create({
-      data: { id: '1' },
-    });
-
-    console.log(test);
+  async getHello(): Promise<string> {
     return this.appService.getHello();
   }
 }
