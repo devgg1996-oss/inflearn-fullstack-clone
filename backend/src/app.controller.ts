d9aa27382b2d1e8a71192c40642917b8b38c6495
devgg1996 <devgg1996@gmail.com>
2025-12-25 13:35:32 +0900

    be - setting - prisma, docker, gitignore

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaClient } from '../generated/prisma';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,12 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    const prisma = new PrismaClient();
+    const test = prisma.test.create({
+      data: { id: '1' },
+    });
+
+    console.log(test);
     return this.appService.getHello();
   }
 }
